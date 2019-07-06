1a0afbf3c44b1ef2e1f14ae6ba9ecd33262aab96
cheerful-transform-7743 <daniel.siekawitch@gmail.com>
2019-07-06 20:03:19 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,9 +1,7 @@
-meal_choice = "What a nutritious meal!"
+
 def meal_choice(veg1, veg2, protein = "meat")
-  {
-    meal_choice
-  }
   "A plate of #{protein} with #{veg1} and #{veg2}."
+  "What a nutritious meal!"
 end
 
 meal_choice("broccoli", "macaroni")
